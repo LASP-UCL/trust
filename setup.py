d3aab85d9f2b745c7e87b09752350e93e4d10a17
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2022-06-30 15:17:04 +0100

    patch gym issue

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from setuptools import setup, find_packages
 
 
+install_requires = [
+    "gym>=0.15,<=0.23",
+]
+
+
+extras_require = {
+
+}
+
+
 setup(
     name="trust",
     description="A Reinforcement Learning library in jax that works.",
@@ -8,5 +18,7 @@ setup(
     author_email="e.pignatelli@ucl.ac.uk",
     url="https://github.com/lasp-ucl/trust",
     packages=find_packages(),
+    install_requires=install_requires,
+    extras_require=extras_require,
     python_requires=">=3.9",
 )
